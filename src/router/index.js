54c09274122ad43ba735eaeb694e96a50fae6e29
Jefferson L. Alionco <Jeffersonalionco@gmail.com>
2025-08-27 16:21:22 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requiresAuth: true, role: 'admin'}
   },
   {
     path: '/about',
@@ -13,12 +14,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { requiresAuth: true, role: 'admin'}
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/LoginView.vue')
+    component: () => import('../views/LoginView.vue'),
+    
   }
 ]
 
@@ -26,5 +29,35 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+// Aqui onde trabalho com a segurança das rotas---
+router.beforeEach((to, from, next) =>{ 
+  
+
+
+  const isLogged = localStorage.getItem("user") !== null;
+  
+  // const user = localStorage.getItem("user")
+  // const tokenDecoded = jwt.verify(user.token, 'SDFBHHLASDGFÇUIHIU4325UIDSF')      /// implementar ainda
+  // console.log(tokenDecoded)
+
+  if(to.meta.requiresAuth && !isLogged){
+    return next('/login')
+  }
+
+  if(to.path === '/login' && isLogged){
+    return next('/')
+  }
+
+
+  //if(to.meta.role && user?.role !== to.meta.role){
+  //  return next('/login')
+ // }
+
+  
+  next()
+})
+
+
 
 export default router
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,6 @@
 <template>
   <!-- Componente de Login -->
   <LoginForms />
-  <p v-show="loading">LOGADO {{ users.email }}</p>
-  <p v-if="error" style="color:red;">{{ error }}</p>
 </template>
 
 <script>
@@ -12,46 +10,10 @@ export default {
   name: "LoginView",
   data() {
     return {
-      users: {}, // 👈 usuário logado (não precisa ser array)
-      loading: false,
-      error: null,
     };
-  },
-  mounted() {
-    this.fetchUsers();
   },
   components: {
     LoginForms,
-  },
-  methods: {
-    async fetchUsers() {
-      this.loading = true;
-      alert("Inicio do try")
-      try {
-        const response = await fetch("http://localhost:3009/api/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: "teste", // 👈 certifique-se que existe no banco
-            senha: "123456",       // 👈 backend espera "password" (não "senha")
-          }),
-        });
-
-        if (!response.ok) throw new Error("Erro ao fazer login");
-
-        const data = await response.json();
-        this.users = data.user; // 👈 supondo que backend retorna { token, user }
-        alert("Login bem-sucedido")
-        console.log("Login bem-sucedido:", data);
-      } catch (err) {
-        alert("Inicio error" + err)
-        this.error = err.message;
-      } finally {
-        this.loading = false;
-      }
-    },
-  },
+  }
 };
 </script>
